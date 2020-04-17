dd539cc680bb2d4251e28633e53c6582807293d8
Kuro <sonichead10@gmail.com>
2020-04-17 21:15:12 +1000

    fixed lerp and deleted camera

--- Magic RTS/src/main/util/Utils.java
+++ Magic RTS/src/main/util/Utils.java
@@ -3,8 +3,13 @@ package main.util;
 public class Utils {
 
 	public static float lerp(float a, float b, float f) {
-
-		return (a * (1.0f - f)) + (b * f);
+		
+		float result = ((a * (1.0f - f)) + (b * f)) * 1000;
+		
+		result = Math.round(result);
+		
+		
+		return result/1000;
 
 	}
 
